3ed1e398ef450c98046ed131ad5df954281d0390
Drake Taylor <drake.taylor@revature.net>
2021-05-12 09:26:36 -0500

    Remove unused import in test

--- src/test/java/com/forge/revature/demo/ProjectTest.java
+++ src/test/java/com/forge/revature/demo/ProjectTest.java
@@ -5,7 +5,6 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Optional;
 
