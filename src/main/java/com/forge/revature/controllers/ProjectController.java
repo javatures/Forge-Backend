76f95b326247fa346a5c834ee59f13b192146864
Drake Taylor <drake.taylor@revature.net>
2021-05-12 17:34:58 -0500

    Implement controller additions

--- src/main/java/com/forge/revature/controllers/ProjectController.java
+++ src/main/java/com/forge/revature/controllers/ProjectController.java
@@ -11,6 +11,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -36,5 +37,31 @@ public class ProjectController {
     @RequestMapping(value = "/projects/{id}", produces = "application/json", method = RequestMethod.GET)
     public ResponseEntity<Optional<Project>> getExperience(@PathVariable(name = "id") long id) {
         return new ResponseEntity<>(repo.findById(id), HttpStatus.OK);
+    }
+
+    @RequestMapping(value = "/projects", consumes = "application/json", method = RequestMethod.POST)
+    public void createExperience(@RequestBody Project proj) {
+        repo.save(proj);
+    }
+
+    @RequestMapping(value = "/projects/{id}", consumes = "application/json", method = RequestMethod.POST)
+    public void updateExperience(@PathVariable(name = "id") long id, @RequestBody Project proj) {
+        Optional<Project> update = repo.findById(id);
+
+        if (update.isPresent()) {
+            update.get().setName(proj.getName());
+            update.get().setDescription(proj.getDescription());
+            update.get().setResponsibilities(proj.getResponsibilities());
+            update.get().setHours(proj.getHours());
+            update.get().setDuration(proj.getDuration());
+
+            proj = update.get();
+            repo.save(proj);
+        }
+    }
+
+    @RequestMapping(value = "/projects/{id}", method = RequestMethod.DELETE)
+    public void deleteExperience(@PathVariable(name = "id") long id) {
+        repo.deleteById(id);
     }
 }
