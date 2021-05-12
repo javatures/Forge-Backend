69ab17a6e9bf93e7e01621dc97af60cb9cc4691c
Drake Taylor <drake.taylor@revature.net>
2021-05-12 17:23:38 -0500

    Implement controller additions

--- src/main/java/com/forge/revature/controllers/WorkExperienceController.java
+++ src/main/java/com/forge/revature/controllers/WorkExperienceController.java
@@ -11,6 +11,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -36,5 +37,33 @@ public class WorkExperienceController {
     @RequestMapping(value = "/workexperience/{id}", produces = "application/json", method = RequestMethod.GET)
     public ResponseEntity<Optional<WorkExperience>> getExperience(@PathVariable(name = "id") long id) {
         return new ResponseEntity<>(repo.findById(id), HttpStatus.OK);
+    }
+
+    @RequestMapping(value = "/workexperience", consumes = "application/json", method = RequestMethod.POST)
+    public void createExperience(@RequestBody WorkExperience work) {
+        repo.save(work);
+    }
+
+    @RequestMapping(value = "/workexperience/{id}", consumes = "application/json", method = RequestMethod.POST)
+    public void updateExperience(@PathVariable(name = "id") long id, @RequestBody WorkExperience work) {
+        Optional<WorkExperience> update = repo.findById(id);
+
+        if (update.isPresent()) {
+            update.get().setEmployer(work.getEmployer());
+            update.get().setTitle(work.getTitle());
+            update.get().setResponsibilities(work.getResponsibilities());
+            update.get().setDescription(work.getDescription());
+            update.get().setTechnologies(work.getTechnologies());
+            update.get().setStartDate(work.getStartDate());
+            update.get().setEndDate(work.getEndDate());
+
+            work = update.get();
+            repo.save(work);
+        }
+    }
+
+    @RequestMapping(value = "/workexperience/{id}", method = RequestMethod.DELETE)
+    public void deleteExperience(@PathVariable(name = "id") long id) {
+        repo.deleteById(id);
     }
 }
