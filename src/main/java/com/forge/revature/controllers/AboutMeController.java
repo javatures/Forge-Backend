c1010bcce0779811ea9a561aeb3ec9b3d6f0b48a
Maxwell Lee <maxwell.lee@revature.net>
2021-05-13 13:07:47 -0700

    Added tests for normal post, findByUser, findByPortfolio for AboutMe and Education

--- src/main/java/com/forge/revature/controllers/AboutMeController.java
+++ src/main/java/com/forge/revature/controllers/AboutMeController.java
@@ -6,7 +6,6 @@ import java.util.Optional;
 import com.forge.revature.models.AboutMe;
 import com.forge.revature.repo.AboutMeRepo;
 
-import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -24,7 +23,8 @@ public class AboutMeController {
     @Autowired
     AboutMeRepo aboutMeRepo;
 
-    public AboutMeController() {}
+    public AboutMeController() {
+    }
 
     public AboutMeController(AboutMeRepo repo) {
         this.aboutMeRepo = repo;
@@ -50,14 +50,11 @@ public class AboutMeController {
     public void updateAboutMe(@RequestBody AboutMe newAboutMe, @PathVariable(name = "id") int aboutMeId) {
         Optional<AboutMe> oldAboutMe = aboutMeRepo.findById(aboutMeId);
 
-        if(oldAboutMe.isPresent())
-        {
-            BeanUtils.copyProperties(newAboutMe, oldAboutMe);
-            /*
+        if (oldAboutMe.isPresent()) {
+
             oldAboutMe.get().setBio(newAboutMe.getBio());
             oldAboutMe.get().setEmail(newAboutMe.getEmail());
             oldAboutMe.get().setPhone(newAboutMe.getPhone());
-            */
 
             aboutMeRepo.save(oldAboutMe.get());
         }
@@ -68,23 +65,21 @@ public class AboutMeController {
         aboutMeRepo.deleteById(aboutMeId);
     }
 
-    @GetMapping("/user/{id}") 
+    @GetMapping("/user/{id}")
     public AboutMe getUserAboutMe(@PathVariable(name = "id") int userId) {
         Optional<AboutMe> retrievedAboutMe = aboutMeRepo.findByPortfolioUserId(userId);
 
-        if(retrievedAboutMe.isPresent())
-        {
+        if (retrievedAboutMe.isPresent()) {
             return retrievedAboutMe.get();
         }
         return null;
     }
 
-    @GetMapping("/portfolio/{id}") 
+    @GetMapping("/portfolio/{id}")
     public AboutMe getPortfolioAboutMe(@PathVariable(name = "id") int portfolioId) {
         Optional<AboutMe> retrievedAboutMe = aboutMeRepo.findByPortfolioId(portfolioId);
 
-        if(retrievedAboutMe.isPresent())
-        {
+        if (retrievedAboutMe.isPresent()) {
             return retrievedAboutMe.get();
         }
         return null;
