6e8ea6891fe6c2cdd0faaa2de9aba12b2295647a
Charles Radcliffe <charles.radcliffe@revature.net>
2021-05-13 13:41:23 -0700

    Cross origins added to controllers that are missing them

--- src/main/java/com/forge/revature/controllers/EducationController.java
+++ src/main/java/com/forge/revature/controllers/EducationController.java
@@ -10,6 +10,7 @@ import com.forge.revature.repo.EducationRepo;
 
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -19,6 +20,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin(origins = "*")
 @RequestMapping("education")
 public class EducationController {
     @Autowired
--- src/main/java/com/forge/revature/controllers/ProjectController.java
+++ src/main/java/com/forge/revature/controllers/ProjectController.java
@@ -16,7 +16,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-@CrossOrigin
+@CrossOrigin(origins = "*")
 @RestController
 public class ProjectController {
     @Autowired
