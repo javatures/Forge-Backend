ea45fce6c934e446a2b00b296bff83f6d183c704
Drake Taylor <drake.taylor@revature.net>
2021-05-20 17:28:44 -0500

    Remove file upload from ProjectController

--- src/main/java/com/forge/revature/controllers/ProjectController.java
+++ src/main/java/com/forge/revature/controllers/ProjectController.java
@@ -1,10 +1,5 @@
 package com.forge.revature.controllers;
 
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.nio.file.StandardCopyOption;
 import java.util.List;
 import java.util.Optional;
 
@@ -14,15 +9,12 @@ import com.forge.revature.repo.ProjectRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.multipart.MultipartFile;
 
 @CrossOrigin(origins = "*")
 @RestController
@@ -53,26 +45,12 @@ public class ProjectController {
     }
 
     @RequestMapping(value = "/projects", consumes = "application/json", method = RequestMethod.POST)
-    public void createExperience(@RequestParam(name = "workproducts", required = false) MultipartFile file, @RequestBody Project proj) {
-        if (file != null) proj.setWorkProducts(StringUtils.cleanPath(file.getOriginalFilename()));
-        Project newProj = repo.save(proj);
-
-        if (file != null) {
-            Path path = Paths.get("src/main/resources/workproducts", Long.toString(newProj.getId()));
-            try {
-                if (!Files.exists(path))
-                    Files.createDirectories(path);
-                Files.copy(file.getInputStream(), path.resolve(StringUtils.cleanPath(file.getOriginalFilename())),
-                        StandardCopyOption.REPLACE_EXISTING);
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        }
+    public void createExperience(@RequestBody Project proj) {
+        repo.save(proj);
     }
 
     @RequestMapping(value = "/projects/{id}", consumes = "application/json", method = RequestMethod.POST)
-    public void updateExperience(@RequestParam(name = "workproducts", required = false) MultipartFile file,
-            @PathVariable(name = "id") long id, @RequestBody Project proj) {
+    public void updateExperience(@PathVariable(name = "id") long id, @RequestBody Project proj) {
         Optional<Project> update = repo.findById(id);
 
         if (update.isPresent()) {
@@ -83,19 +61,6 @@ public class ProjectController {
             update.get().setRespositoryUrl(proj.getRespositoryUrl());
             update.get().setWorkProducts(proj.getWorkProducts());
             update.get().setPortfolio(proj.getPortfolio());
-
-            if (file != null) {
-                update.get().setWorkProducts(StringUtils.cleanPath(file.getOriginalFilename()));
-                Path path = Paths.get("src/main/resources/workproducts", Long.toString(update.get().getId()));
-                try {
-                    if (!Files.exists(path))
-                        Files.createDirectories(path);
-                    Files.copy(file.getInputStream(), path.resolve(StringUtils.cleanPath(file.getOriginalFilename())),
-                            StandardCopyOption.REPLACE_EXISTING);
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
 
             proj = update.get();
             repo.save(proj);
